7ca535f7ca92b44803a0fc47780dde4b8b39db95
eundonghyeok <mephisto7191@naver.com>
2024-07-08 17:27:28 +0900

    Onebite-section5

---
+++ section5/src/components/Editor.jsx
@@ -0,0 +1,41 @@
+import { useState, useRef } from "react";
+import "./Editor.css";
+
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeyDown = (e) => {
+    if (e.KeyCode === 13) {
+      onsubmit();
+    }
+  };
+
+  const onsubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onsubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
