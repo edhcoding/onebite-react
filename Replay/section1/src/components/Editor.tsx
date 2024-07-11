0902c1aa883364610054c135a7c4f54f0215afb8
eundonghyeok <mephisto7191@naver.com>
2024-07-11 17:18:35 +0900

    todolist - create(todo 추가하기)

--- Replay/section1/src/components/Editor.tsx
+++ Replay/section1/src/components/Editor.tsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Editor.css";
 
-export default function Editor() {
+export default function Editor({ onCreate }) {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onKeydown={onKeydown}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 }
