8230f38d05093a9c6138542b04ae93d1c72e6d13
eundonghyeok <mephisto7191@naver.com>
2024-07-11 18:39:11 +0900

    todolist - 투두 수정하기

--- Replay/section1/src/components/TodoItem.jsx
+++ Replay/section1/src/components/TodoItem.jsx
@@ -3,10 +3,14 @@ import "./TodoItem.css";
 // toDateString Thu Jul 11 2024 년도까지만 끝 시간 안나옴
 // toLocaleDateString 2024.7.11 이렇게 나옴
 
-export default function TodoItem({ id, isDone, content, date }) {
+export default function TodoItem({ id, isDone, content, date, onUpdate }) {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
   return (
     <div className="TodoItem">
-      <input checked={isDone} type="checkbox" />
+      <input onChange={onChangeCheckbox} checked={isDone} type="checkbox" />
       <div className="content">{content}</div>
       <div className="date">{new Date(date).toLocaleDateString()}</div>
       <button>삭제</button>
