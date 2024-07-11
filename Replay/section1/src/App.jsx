7dbb078a2a23b9f7a4f2522ccec16fb304a1ae4f
eundonghyeok <mephisto7191@naver.com>
2024-07-11 16:58:08 +0900

    todolist 기능구현 준비하기

--- Replay/section1/src/App.jsx
+++ Replay/section1/src/App.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
+
 // npm create vite@latest로 설치
 function App() {
+  const [todos, setTodos] = useState([mockData]);
+
   return (
     <div className="App">
       <Header />
