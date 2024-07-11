8f676496ddc580a6480b9f6d18dde2cddc71742e
eundonghyeok <mephisto7191@naver.com>
2024-07-12 03:19:14 +0900

    react-router-dom 기초세팅

--- Replay2/src/main.jsx
+++ Replay2/src/main.jsx
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// BrowserRouter - 브라우저의 현재 주소를 저장하고 감지하는 역할
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
