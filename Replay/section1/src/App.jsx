ed11dfe5503c7d791c6dfe91a311a6a5d34f8e11
eundonghyeok <mephisto7191@naver.com>
2024-07-11 16:52:14 +0900

    UI 구현하기

--- Replay/section1/src/App.jsx
+++ Replay/section1/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import Editor from "./components/Editor";
+import Header from "./components/Header";
+import List from "./components/List";
 
 // npm create vite@latest로 설치
 function App() {
   return (
-    <>
-      <div>기초세팅</div>
-    </>
+    <div className="App">
+      <Header />
+      <Editor />
+      <List />
+    </div>
   );
 }
 
