f2b9fa995e0a40a5b7f62fe395f7c070b1ca75ac
eundonghyeok <mephisto7191@naver.com>
2024-07-12 19:16:05 +0900

    일기 관리 기능 구현하기1

--- Replay2/src/App.jsx
+++ Replay2/src/App.jsx
@@ -3,9 +3,9 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
-import Button from "./components/Button";
-import Header from "./components/Header";
+import Edit from "./pages/Edit";
 import "./App.css";
+import { useReducer } from "react";
 
 // 1. / - 모든 일기를 조회하는 Home 페이지
 // 2. /new - 새로운 일기를 작성하는 New 페이지
@@ -15,23 +15,35 @@ import "./App.css";
 // <Link to={"/"}>Home</Link>
 // useNavigate - 함수를 이용해서 특정 이벤트가 발생했을 때 페이지를 이동시키는 방법
 
-function App() {
-  return (
-    <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"Left"} />}
-        rightChild={<Button text={"Right"} />}
-      />
-      <Button />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
-    </>
-  );
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "2번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  return state;
 }
 
-export default App;
+export default function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary/:id" element={<Diary />} />
+      <Route path="/edit/:id" element={<Edit />} />
+      <Route path="*" element={<Notfound />} />
+    </Routes>
+  );
+}
