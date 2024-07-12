966c89ab37b380f9dc2b94272a04ecdbe2c4e212
eundonghyeok <mephisto7191@naver.com>
2024-07-12 17:52:49 +0900

    동적 경로 사용법

--- Replay2/src/pages/Diary.jsx
+++ Replay2/src/pages/Diary.jsx
@@ -1,3 +1,13 @@
+import { useParams, useSearchParams } from "react-router-dom";
+
 export default function Diary() {
+  // const params = useParams();
+  // diary/1 /2 /3 urlParameter일때 사용
+  // console.log(params);  id: 100   ex)사용할때는 {params.id} 100번 일기입니다!
+
+  const [params, setParams] = useSearchParams();
+  // diray?value=hi 쿼리스트링 일 때 사용
+  // console.log(params.get("value")); // hi
+
   return <div>Diary</div>;
 }
