6dcca57e7841e47a66833073c59de9d824ebe610
eundonghyeok <mephisto7191@naver.com>
2024-07-13 20:17:19 +0900

    Diary page 구현하기

--- Replay2/src/components/Editor.jsx
+++ Replay2/src/components/Editor.jsx
@@ -1,48 +1,10 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { emotionList } from "../util/constants";
+import { getStringedDate } from "../util/get-stringed-date";
 import Button from "./Button";
 import EmotionItem from "./EmotionItem";
 import "./Editor.css";
-
-const emotionList = [
-  {
-    emotionId: 1,
-    emotionName: "완전 좋음",
-  },
-  {
-    emotionId: 2,
-    emotionName: "좋음",
-  },
-  {
-    emotionId: 3,
-    emotionName: "그럭저럭",
-  },
-  {
-    emotionId: 4,
-    emotionName: "나쁨",
-  },
-  {
-    emotionId: 5,
-    emotionName: "끔찍함",
-  },
-];
-
-const getStringedDate = (targetDate) => {
-  // 날짜 => YYYY-MM-DD 형태로 반환
-  let year = targetDate.getFullYear();
-  let month = targetDate.getMonth() + 1;
-  let date = targetDate.getDate();
-
-  if (month < 10) {
-    month = `0${month}`;
-  }
-
-  if (date < 10) {
-    date = `0${date}`;
-  }
-
-  return `${year}-${month}-${date}`;
-};
 
 export default function Editor({ initData, onSubmit }) {
   // 하나의 input에 날짜, 감정, 일기 전부 저장할거임
--- Replay2/src/pages/Diary.jsx
+++ Replay2/src/pages/Diary.jsx
@@ -1,12 +1,42 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { getStringedDate } from "../util/get-stringed-date";
+import useDiary from "../hooks/useDiary";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Viewer from "../components/Viewer";
+
 export default function Diary() {
-  return <div>Diary</div>;
+  const params = useParams();
+  const nav = useNavigate();
+
+  const curDiaryItem = useDiary(params.id);
+
+  if (!curDiaryItem) {
+    return <div>데이터 로딩중...</div>;
+  }
+
+  const { createdDate, emotionId, content } = curDiaryItem;
+
+  const title = getStringedDate(new Date(createdDate));
+
+  return (
+    <div>
+      <Header
+        title={`${title} 기록`}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로가기"} />}
+        rightChild={
+          <Button onClick={() => nav(`/edit/${params.id}`)} text={"수정하기"} />
+        }
+      />
+      <Viewer emotionId={emotionId} content={content} />
+    </div>
+  );
 }
 
-
 // const params = useParams();
-  // diary/1 /2 /3 urlParameter일때 사용
-  // console.log(params);  id: 100   ex)사용할때는 {params.id} 100번 일기입니다!
+// diary/1 /2 /3 urlParameter일때 사용
+// console.log(params);  id: 100   ex)사용할때는 {params.id} 100번 일기입니다!
 
-  // const [params, setParams] = useSearchParams();
-  // diray?value=hi 쿼리스트링 일 때 사용
-  // console.log(params.get("value")); // hi
+// const [params, setParams] = useSearchParams();
+// diray?value=hi 쿼리스트링 일 때 사용
+// console.log(params.get("value")); // hi
